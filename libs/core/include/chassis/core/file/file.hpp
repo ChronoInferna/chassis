13b0a6f0779cf31de58986d14ee27cf35b117281
Oliver Deng <oliver.t.deng@gmail.com>
2026-07-30 16:25:36 -0400

    refactor: make the refactor actually work

--- libs/core/include/chassis/core/file/file.hpp
+++ libs/core/include/chassis/core/file/file.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <concepts>
+
 namespace chassis::file {
 
 struct File {};
@@ -7,5 +9,8 @@ struct File {};
 struct TextFile : public File {};
 
 struct BinaryFile : public File {};
+
+template <typename F = File>
+concept is_binary_file = std::same_as<F, BinaryFile>;
 
 } // namespace chassis::file
--- libs/filesystem/include/chassis/filesystem/io.hpp
+++ libs/filesystem/include/chassis/filesystem/io.hpp
@@ -4,6 +4,7 @@
 #include <chassis/filesystem/path.hpp>
 
 #include <concepts>
+#include <fstream>
 
 namespace chassis::fs {
 
@@ -16,13 +17,53 @@ enum class FileWriteMode {
 };
 
 template <typename F = File>
-  requires std::derived_from<F, File>
-auto read_file(const path &p) -> Result<F>;
+  requires(std::derived_from<F, File> && !is_binary_file<F>)
+auto read_file(const path &p) -> Result<F> {
+  std::ifstream file{p};
+  if (!file.is_open()) {
+    return make_error(ErrorCode::InvalidArgument);
+  }
+
+  File res{};
+  std::string buffer;
+  while (std::getline(file, buffer)) {
+    // TODO
+  }
+
+  return res;
+}
 
 template <typename F = File>
-  requires std::derived_from<F, File>
+  requires(std::derived_from<F, File> && !is_binary_file<F>)
 auto write_file(const path &p, F input_file,
-                FileWriteMode mode = FileWriteMode::Append) -> Result<void>;
+                FileWriteMode mode = FileWriteMode::Append) -> Result<void> {
+  std::ofstream file;
+
+  if (mode == FileWriteMode::Append) {
+    file.open(p, std::ios::app);
+  } else if (mode == FileWriteMode::Overwrite) {
+    file.open(p, std::ios::trunc);
+  } else {
+    return make_error(ErrorCode::InvalidArgument);
+  }
+
+  if (!file.is_open()) {
+    return make_error(ErrorCode::InvalidArgument);
+  }
+
+  // TODO
+  // for (const auto &line : input_file.content()) {
+  //   file << line << "\n";
+  // }
+
+  file.close();
+  return {};
+}
+
+// TODO
+// template <typename F = File>
+//   requires (std::derived_from<F, File> && is_binary_file<F>)
+//   auto write_binary_file();
 
 // TODO
 // auto atomic_write_file(const path &p, File input_file) -> Result<void>;
